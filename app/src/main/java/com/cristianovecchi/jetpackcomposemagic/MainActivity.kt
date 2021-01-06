c5f12ef6cb214f30af3d6311fe4313a58ac7b096
Cristiano Vecchi <crisvecchi2016@gmail.com>
2021-01-06 02:25:37 +0100

    Initial commit

---
+++ app/src/main/java/com/cristianovecchi/jetpackcomposemagic/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.cristianovecchi.jetpackcomposemagic
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
+import com.cristianovecchi.jetpackcomposemagic.ui.JetpackComposeMagicTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeMagicTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello ${name}!")
+}
+
+@Composable
+fun Counter() {
+    val count = remember { mutableStateOf(0)}
+    Button(onClick =  { count.value++}){
+        Text("I've been clicked ${count.value} times.")
+    }
+}
+
+@Composable
+fun SimpleList(names: List<String> = listOf("Kotlin", "Java", "Scala")){
+    Column{
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeMagicTheme {
+        Column{
+            SimpleList()
+            Counter()
+        }
+    }
+}
