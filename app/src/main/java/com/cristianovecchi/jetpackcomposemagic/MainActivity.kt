bd60c9586567c33b3897011217f245692f52f042
Cristiano Vecchi <crisvecchi2016@gmail.com>
2021-01-14 00:47:09 +0100

    gradle.properties changed

--- app/src/main/java/com/cristianovecchi/jetpackcomposemagic/MainActivity.kt
+++ app/src/main/java/com/cristianovecchi/jetpackcomposemagic/MainActivity.kt
@@ -2,7 +2,7 @@ package com.cristianovecchi.jetpackcomposemagic
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
+import androidx.compose.material.Text
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
